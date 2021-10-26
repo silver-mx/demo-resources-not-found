991891cd39ef8c3ff362d0c0b416c5d4dbc53782
Diego Núñez Silva <diego.nunez.mx@gmail.com>
2021-10-26 20:07:07 +0200

    Initial commit

---
+++ src/test/java/com/example/demoresourcesnotfound/DemoResourcesNotFoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoresourcesnotfound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoResourcesNotFoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
